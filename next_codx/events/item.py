8777dc8d7ff77512f5f1c66bbc61fd61adc1526d
Omneya <monaeid9334@gmail.com>
2024-08-19 22:20:37 +0200

    apply some fixes

---
+++ next_codx/events/item.py
@@ -0,0 +1,3 @@
+def set_supplier(doc, method=None):
+    if doc.has_variants == 0:
+        doc.default_supplier = frappe.db.get_value(doc.doctype, doc.variant_of, "default_supplier")
---
+++ next_codx/patches/post_model_sync/item_custom_fields.py
@@ -0,0 +1,31 @@
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+import frappe
+
+def execute():
+    setup_custom_fields()
+
+def setup_custom_fields():
+    custom_fields = {
+        "Item": [
+            dict(
+                fieldname="default_supplier",
+                label="Default Supplier",
+                fieldtype="Link",
+                options="Supplier",
+                insert_after="stock_uom",
+                print_hide=1,
+            )
+        ],
+        "Sales Order Item": [
+            dict(
+                fieldname="supplier",
+                label="Supplier",
+                fieldtype="Link",
+                options="Supplier",
+                fetch_from="item_code.default_supplier",
+                insert_after="delivered_by_supplier",
+            )
+        ],
+    }
+
+    create_custom_fields(custom_fields, ignore_validate=True)
